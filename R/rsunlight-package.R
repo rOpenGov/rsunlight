29bdfcf31b40de6f33d6076d8e106240b892b6c8
leeper <thosjleeper@gmail.com>
2014-05-09 00:13:23 +0200

    fix typo

--- R/rsunlight-package.R
+++ R/rsunlight-package.R
@@ -15,7 +15,7 @@
 #'  \item Sunlight Labs Open States API (`os`)
 #'  \item Sunlight Labs Real Time Congress API (`rt`)
 #'  \item Sunlight Labs Capitol Words API (`cw`)
-#'  \itme Sunlight Labs Influence Explorer API (`ie`)
+#'  \item Sunlight Labs Influence Explorer API (`ie`)
 #' }
 #'
 #' Note that Puerto Rico is not included in Sunlight Foundation data.
