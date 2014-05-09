a3d11cc9daf6b9402dad0111063d38034675795e
Scott Chamberlain <myrmecocystus@gmail.com>
2014-05-08 22:01:57 -0700

    vectorized os_statemetasearch, added curlopts param

--- R/os_statemetasearch.R
+++ R/os_statemetasearch.R
@@ -2,24 +2,38 @@
 #'
 #' @import httr
 #' @importFrom plyr compact
-#' @param state state two-letter abbreviation (character)
-#' @param key your SunlightLabs API key; loads from .Rprofile
-#' @return List of output fields.
+#' @param state One or more two-letter state abbreviations (character)
+#' @param key your SunlightLabs API key; or loads from .Rprofile
+#' @param callopts Curl options passed on to \link[httr]{\code{GET}}
+#' @return A list with metadata for each state.
 #' @export
 #' @examples \dontrun{
+#' os_statemetasearch()
 #' os_statemetasearch('ca')
+#' os_statemetasearch('tx')
+#' out <- os_statemetasearch(c('tx','nv'))
+#' out$tx
+#' out$nv
+#' os_statemetasearch('tx', verbose())
 #' }
-os_statemetasearch <- function(state = NULL,
-    key = getOption("SunlightLabsKey", stop("need an API key for Sunlight Labs")))
+os_statemetasearch <- function(state = NULL, 
+    key = getOption("SunlightLabsKey", stop("need an API key for Sunlight Labs")), callopts=list())
 {
-  if(is.null(state))
-    stop("state must specify a two letter state abbreviation")
-  url = "http://openstates.org/api/v1/metadata/"
-  url_ <- paste(url, state, "/?apikey=", key, sep= "")
-  tt <- GET(url_)
-  stop_for_status(tt)
-  out <- content(tt, as = "text")
-  res <- fromJSON(out, simplifyVector = FALSE)
+  getdata <- function(x=NULL){  
+    url = "http://openstates.org/api/v1/metadata/"
+    if(!is.null(x))
+      url <- paste(url, x, sep= "")
+    url2 <- paste(url, "?apikey=", key, sep= "")
+    tt <- GET(url2, callopts)
+    stop_for_status(tt)
+    out <- content(tt, as = "text")
+    fromJSON(out, simplifyVector = FALSE)
+  }
+  if(!is.null(state)){
+    res <- lapply(state, getdata)
+    names(res) <- state
+  } else { res <- getdata() }
+  
   class(res) <- "os_statemetasearch"
   return( res )
 }
