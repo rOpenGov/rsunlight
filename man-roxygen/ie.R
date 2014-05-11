de067fe96396be08fe9d6096da151bc9e9cecd06
Scott Chamberlain <myrmecocystus@gmail.com>
2014-05-11 07:05:57 -0700

    edits to man files for some ie_ fxns

--- man-roxygen/ie.R
+++ man-roxygen/ie.R
@@ -1,4 +1,5 @@
-#' @param key your SunlightLabs API key; loads from .Rprofile
 #' @param page The page of results to return; defaults to 1.
-#' @param per_page The number of results to return per page, defaults to 1,000. The maximum number of records per page is 100,000.
+#' @param per_page The number of results to return per page, defaults to 1,000.
+#' The maximum number of records per page is 100,000.
+#' @param key your SunlightLabs API key; loads from .Rprofile
 #' @param callopts Optional additional curl options (debugging tools mostly)
