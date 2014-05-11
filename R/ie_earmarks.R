81b92a93ed79213061b5c52ac191ee5bfb8d9de1
Scott Chamberlain <myrmecocystus@gmail.com>
2014-05-11 08:08:28 -0700

    added ie_earmarks fxn

---
+++ R/ie_earmarks.R
@@ -0,0 +1,43 @@
+#' Search itemized earmark requests through FY 2010.
+#' 
+#' @import httr
+#' @param amount (integer) The final amount of the earmark.
+#' @param bill (character) The bill, section or subsection of the earmark.
+#' @param city (character) The city where the money will be spent.
+#' @param description (character) Full-text search on the description of the earmark request.
+#' @param member (character) Full-text search on the member of Congress requesting the earmark.
+#' @param member_party (character) The party of the member requesting the earmark, D, R or I.
+#' @param member_state (character) The 2-letter state abbreviation of the requesting member.
+#' @param recipient (character) Full-text search on the intended recipient, when known.
+#' @param year (character) The YYYY-formatted fiscal year for which the earmark was requested.
+#' @template ie
+#' @template earmarks
+#' @return A list, as class ie_earmarks.
+#' @export
+#' @examples \dontrun{
+#' ie_earmarks(year='2010', per_page=3)
+#' ie_earmarks(city='New York City', per_page=1)
+#' ie_earmarks(member='Nadler', per_page=1)
+#' ie_earmarks(member_state='AK', per_page=3)
+#' ie_earmarks(member_party='R', per_page=1)
+#' ie_earmarks(description='Infantry Support')
+#' }
+
+ie_earmarks <- function(amount = NULL, bill = NULL, city = NULL, description = NULL, member = NULL, 
+  member_party = NULL, member_state = NULL, recipient = NULL, year = NULL, page = NULL, 
+  per_page = NULL, key=getOption("SunlightLabsKey", stop("need an API key for Sunlight Labs")),
+  callopts = list()) 
+{
+  url <- "http://transparencydata.com/api/1.0/earmarks.json"
+  args <- suncompact(list(apikey = key, amount = amount, bill = bill, city = city, 
+    description = description, member = member, member_party = member_party, 
+    member_state = member_state, recipient = recipient, year = year, page = page, 
+    per_page = per_page))
+  tt <- GET(url, query=args, callopts)
+  stop_for_status(tt)
+  assert_that(tt$headers$`content-type` == 'application/json; charset=utf-8')
+  out <- content(tt, as = "text")
+  res <- fromJSON(out, simplifyVector = FALSE)
+  class(res) <- "ie_earmarks"
+  return( res )
+}
---
+++ man-roxygen/earmarks.R
@@ -0,0 +1,37 @@
+#' @return THe output fields for each element are as follows.
+#' 
+#' \itemize{ 
+#'  \item fiscal_year The fiscal year for the bill in which the earmark appears. 
+#'  \item final_amount The earmark amount in the final version of the bill. 
+#'  \item budget_amount The earmark amount in the President's budget proposal. 
+#'  \item house_amount The earmark amount in the version of the bill passed by the House. 
+#'  \item senate_amount The earmark amount in the version of the bill passed by the Senate. 
+#'  \item omni_amount The earmark amount in the omnibus appropriations bill. 
+#'  \item bill, bill_section, bill_subsection The bill, section and subsection where the earmark 
+#'  appears. 
+#'  \item description The earmark description. 
+#'  \item notes Notes added by Taxpayers for Common Sense. 
+#'  \item presidential Presidential earmarks are earmarks that appear in the President's initial 
+#'  budget proposal.
+#'   \tabular{ll}{
+#'     blank \tab Not in the President's budget.\cr
+#'     p \tab Included in the President's budget and disclosed by congress. R\cr
+#'     u \tab Included in the President's budget and not disclosed by congress.\cr
+#'     m \tab Included in the President's budget and sponsored by members.\cr
+#'     j \tab Included at the request of the judiciary. 
+#'   }
+#'  \item undisclosed Taxpayers for Common Sense's determination of whether the earmarks was 
+#'  disclosed by congress. Undisclosed earmarks are found by reading the bill text.
+#'   \tabular{ll}{
+#'     blank \tab Disclosed in congressional earmark reports.\cr
+#'     u \tab Not disclosed by congress but found in bill text.\cr
+#'     p \tab Not disclosed and in President's budget.\cr
+#'     o \tab Disclosed Operations & Maintenance earmark.\cr
+#'     m \tab tab Undisclosed Operations & Maintenance earmark. 
+#'   }
+#'  \item members The members that sponsored the earmark. Taxpayers for Common Sense lists the 
+#'  members by last name, state and party. We have attempted to expand these to full names, where 
+#'  possible. Due to formatting irregularities, state, party or full name are sometimes missing. 
+#'  \item location The city and states where the earmark will be spent, when known. 
+#'  \item recipients The organization that will receive the earmark, when known
+#' }
