fc39a4ef936d134bae0159dbd7c0a3d2ee405105
Scott Chamberlain <myrmecocystus@gmail.com>
2014-05-11 09:05:25 -0700

    added ie_epa and ie_faca fxns

---
+++ R/ie_epa.R
@@ -0,0 +1,37 @@
+#' Search itemized EPA enforcement actions.
+#' 
+#' @import httr
+#' @param case_name (character) The name of the enforcement case.
+#' @param case_num (character) One or more specific case numbers.
+#' @param defendants (character) Full-text search for the name(s) of the defendant companies.
+#' @param first_date (character) Start of a date range of the most recent date of significance to 
+#' the case in ISO format.
+#' @param last_date (character) End of a date range of the most recent date of significance to the 
+#' case in ISO format.
+#' @param location_adresses (character) Full-text search on all addresses associated with the case.
+#' @param penalty (integer) Total penalties, in US dollars.
+#' @template ie
+#' @return A list, as class ie_epa.
+#' @export
+#' @examples \dontrun{
+#' ie_epa(defendants='Massey Energy', per_page=1)
+#' ie_epa(defendants='Massey Energy', first_date='>|2005-01-01')
+#' }
+
+ie_epa <- function(case_name = NULL, case_num = NULL, defendants = NULL, first_date = NULL, 
+  last_date = NULL, location_addresses = NULL, penalty = NULL, page = NULL, 
+  per_page = NULL, key=getOption("SunlightLabsKey", stop("need an API key for Sunlight Labs")),
+  callopts = list()) 
+{
+  url <- "http://transparencydata.com/api/1.0/epa.json"
+  args <- suncompact(list(apikey = key, case_name = case_name, case_num = case_num, 
+    defendants = defendants, first_date = first_date, last_date = last_date, 
+    location_addresses = location_addresses, penalty = penalty, page = page, per_page = per_page))
+  tt <- GET(url, query=args, callopts)
+  stop_for_status(tt)
+  assert_that(tt$headers$`content-type` == 'application/json; charset=utf-8')
+  out <- content(tt, as = "text")
+  res <- fromJSON(out, simplifyVector = FALSE)
+  class(res) <- "ie_epa"
+  return( res )
+}
---
+++ R/ie_faca.R
@@ -0,0 +1,36 @@
+#' Search for itemized Federal Advisory Committee memberships.
+#' 
+#' @import httr
+#' @param affiliation (character) The name of the affiliated organization.
+#' @param agency_name (character) The name of the agency associated with the committee.
+#' @param committee_name (character) The name of the advisory committee.
+#' @param member_name (character) Full-text search on the name of the affiliated organization.
+#' @param year (integer) The YYYY-formatted year(s) the member sat on the committee.
+#' @template ie
+#' @return A list, as class ie_faca.
+#' @export
+#' @examples \dontrun{
+#' ie_faca(member_name='Francis Collins', per_page=1)
+#' ie_faca(agency_name='DOC', per_page=3)
+#' ie_faca(affiliation='U.S. House of Representatives', per_page=3)
+#' ie_faca(year=2011, per_page=1)
+#' ie_faca(committee_name='2010 Census Advisory Committee', per_page=1)
+#' }
+
+ie_faca <- function(affiliation = NULL, agency_name = NULL, committee_name = NULL, member_name = NULL, 
+  year = NULL, page = NULL, per_page = NULL, 
+  key=getOption("SunlightLabsKey", stop("need an API key for Sunlight Labs")),
+  callopts = list()) 
+{
+  url <- "http://transparencydata.com/api/1.0/faca.json"
+  args <- suncompact(list(apikey = key, affiliation = affiliation, agency_name = agency_name, 
+    committee_name = committee_name, member_name = member_name, year=year, page = page, 
+    per_page = per_page))
+  tt <- GET(url, query=args, callopts)
+  stop_for_status(tt)
+  assert_that(tt$headers$`content-type` == 'application/json; charset=utf-8')
+  out <- content(tt, as = "text")
+  res <- fromJSON(out, simplifyVector = FALSE)
+  class(res) <- "ie_faca"
+  return( res )
+}
