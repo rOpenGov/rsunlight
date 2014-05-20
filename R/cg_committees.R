b50e9de16bbcaf32df6d2d3435a83003ced54baa
Scott Chamberlain <myrmecocystus@gmail.com>
2014-05-20 09:58:04 -0700

    fixed up cg_leg and cg_comm and sun_df fxns decided to just return lists to match rest of pkg instead of httr responses

--- R/cg_committees.R
+++ R/cg_committees.R
@@ -6,21 +6,22 @@
 #' bugs or suggestions. We only provide information on current committees and memberships. For 
 #' historic data on committee names, IDs, and contact info, refer to the bulk data.
 #' 
-#' @import httr
+#' @import httr assertthat
 #' @template cg
 #' @param id committee id (eg. JSPR)
 #' @return Committee details including subcommittees and all members.
 #' @export
 #' @examples \dontrun{
-#' cg_committees(member_ids='L000551')
-#' cg_committees(committee_id='SSAP')
-#' cg_committees(committee_id='SSAP', fields='members')
-#' cg_committees(chamber='joint', subcommittee=FALSE)
-#' cg_committees(chamber='joint', subcommittee=TRUE)
-#' cg_committees(parent_committee_id='HSWM')
+#' out <- cg_committees(member_ids='L000551')
+#' out <- cg_committees(committee_id='SSAP')
+#' out <- cg_committees(committee_id='SSAP', fields='members')
+#' out <- cg_committees(chamber='joint', subcommittee=FALSE)
+#' out <- cg_committees(parent_committee_id='HSWM')
+#' sun_df(out)
 #' 
 #' # Disable pagination
-#' cg_committees(per_page='all')
+#' out <- cg_committees(per_page='all')
+#' sun_df(out)
 #' }
 
 cg_committees <-  function(member_ids = NULL, committee_id = NULL, chamber = NULL, 
@@ -31,16 +32,33 @@ cg_committees <-  function(member_ids = NULL, committee_id = NULL, chamber = NUL
   if(!is.null(subcommittee))
     subcommittee <- ifelse(subcommittee, 'true', 'false')
   url = "https://congress.api.sunlightfoundation.com/committees"
+  fields <- paste0(fields, collapse = ",")
   args <- suncompact(list(apikey = key, member_ids = member_ids, committee_id = committee_id, 
               chamber = chamber, subcommittee = subcommittee, fields = fields,
               parent_committee_id = parent_committee_id, page = page, per_page=per_page))
   tt <- GET(url, query=args, callopts)
   stop_for_status(tt)
   assert_that(tt$headers$`content-type` == 'application/json; charset=utf-8')
-#   out <- content(tt, as = "text")
-#   res <- fromJSON(out, simplifyVector = FALSE)$response
-#   class(res) <- "cg_getcommittees"
-  message(sprintf("Found %s records", res$found))
-  class(tt) <- c("response","cg_committees")
-  return( tt )
+  out <- content(tt, as = "text")
+  res <- fromJSON(out, simplifyVector = FALSE)
+  class(res) <- "cg_committees"
+#   message(sprintf("Found %s records", content(tt)$found))
+#   class(tt) <- c("response","cg_committees")
+#   class(tt) <- c("cg_committees","response")
+  return( res )
 }
+
+# #' Print cg_committees output
+# #' @export
+# #' @param x Input from a cg_committees function call
+# #' @param ... Further args, ignored.
+# print.cg_committees <- function(x, ...){
+# #   xx <- content(x)
+#   query <- parse_url(x$url)
+#   cat(sprintf("Call status: %s", x$status_code), "\n")
+#   cat("First few query parameters:", "\n")
+#   for(i in seq_along(query$query[1:length(query$query)])){
+#     cat(sprintf("  %s = %s", names(query$query)[i], query$query[[i]]), "\n")
+#   }
+#   cat(sprintf("Found %s records", content(x)$count))
+# }
--- R/cg_legislators.R
+++ R/cg_legislators.R
@@ -17,7 +17,7 @@
 #' out <- cg_legislators(twitter_id = 'SenRandPaul')
 #' out <- cg_legislators(facebook_id = 'mitchmcconnell')
 #' out <- cg_legislators(latitude = 35.778788, longitude = -78.787805)
-#' sunlight_list(out)
+#' sun_df(out)
 #' out <- cg_legislators(zip = 77006)
 #' }
 cg_legislators <- function(title=NULL, first_name=NULL, middle_name=NULL,
@@ -67,69 +67,11 @@ cg_legislators <- function(title=NULL, first_name=NULL, middle_name=NULL,
   tt <- GET(url, query=args, callopts)
   stop_for_status(tt)
   assert_that(tt$headers$`content-type` == 'application/json; charset=utf-8')
-#   out <- content(tt, as = "text")
-#   res <- fromJSON(out, simplifyVector = FALSE)
+  out <- content(tt, as = "text")
+  res <- fromJSON(out, simplifyVector = FALSE)
 #   res <- list(found = res$count, meta = res$page,
 #               data = if(length(res$results) == 1){ res$results[[1]] } else { res$results } )
-  message(sprintf("Found %s records", res$found))
-  class(tt) <- c("response","cg_legislators")
-  return( tt )
-}
-
-sunlight_list <- function(x){
-  assert_that(is(x, "response"))
-  out <- content(x, as = "text")
-  res <- fromJSON(out, simplifyVector = FALSE)
-  resout <- list(found = res$count, meta = res$page,
-       data = if(length(res$results) == 1){ res$results[[1]] } else { res$results } )
-  class(resout) <- class(x)[ !class(x) %in% "response" ]
-  return( resout )
-}
-
-#' Write data from any rsunlight function output to a csv file on your machine.
-#' 
-#' @import assertthat
-#' @export
-#' @param x Output from any of the rsunlight functions.
-#' @param ... Further args passed to read.csv
-#' @details This function attemps to coerce the raw output from each rsunlight function to a 
-#' data.frame to write to csv, but it may fail in some cases. You can always make your own 
-#' data.frame. 
-#' @examples \dontrun{
-#' out <- cg_legislators(latitude = 35.778788, longitude = -78.787805)
-#' sunlight_df(sunlight_list(out))
-#' sunlight_df(out)
-#' 
-#' out <- cg_committees(member_ids='L000551')
-#' sunlight_df(sunlight_list(out))
-#' }
-sunlight_df <- function(x, ...){
-  UseMethod("sunlight_df")
-}
-
-sunlight_df.cg_legislators <- function(x){
-  assert_that(typeof(x) == "list")
-  if(x$found == 1){
-    iter <- x$data
-    df <- data.frame(iter, stringsAsFactors = FALSE)
-  } else {
-    iter <- x$data
-    iter <- lapply(iter, replacemissing)
-    df <- do.call(rbind.fill, lapply(iter, data.frame, stringsAsFactors = FALSE))
-  }
-  return( df )
-}
-
-sunlight_df.cg_committees <- function(x){
-  assert_that(is(x, "cg_committees"))
-  notmembers <- x$committee[!names(x$committee) %in% "members"]
-  notmembers <- replacemissing(notmembers)
-  members <- do.call(rbind.fill, lapply(x$committee$members, data.frame, stringsAsFactors = FALSE))
-  cbind(notmembers, members)
-}
-
-replacemissing <- function(x){
-  lapply(x, function(y){
-    ifelse(length(y)==0, "", y)
-  })
+  class(res) <- "cg_legislators"
+#   class(tt) <- c("response","cg_legislators")
+  return( res )
 }
