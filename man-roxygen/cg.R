93a393a4efb0f6bcb61fdc0229034bf9e4a0271e
Scott Chamberlain <myrmecocystus@gmail.com>
2014-08-08 07:15:06 -0700

    added new param return to cg template

--- man-roxygen/cg.R
+++ man-roxygen/cg.R
@@ -1,2 +1,3 @@
 #' @param key your SunlightLabs API key; loads from .Rprofile
+#' @param return (character) One of table (default), list, or response (httr response object)
 #' @param callopts Optional additional curl options (debugging tools mostly)
