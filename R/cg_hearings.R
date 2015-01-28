4666a2fa42669a4b1d53351f92ff7d8343cdcffc
Scott Chamberlain <myrmecocystus@gmail.com>
2015-01-27 20:56:24 -0800

    fixed man file things

--- R/cg_hearings.R
+++ R/cg_hearings.R
@@ -3,10 +3,14 @@
 #' @export
 #'
 #' @param committee_id (numeric) The ID of the committee holding the hearing.
-#' @param occurs_at (character) The time the hearing will occur. format: "%Y-%m-%dT%H:%M:%SZ" e.g. "2014-05-01T13:00:00Z" 
-#' @param congress (numeric) The number of the Congress the committee hearing is taking place during.
-#' @param chamber (character) The chamber ('house', 'senate', or 'joint') of the committee holding the hearing.
-#' @param dc (logical) Whether the committee hearing is held in DC (TRUE) or in the field (FALSE).
+#' @param occurs_at (character) The time the hearing will occur. format: "%Y-%m-%dT%H:%M:%SZ"
+#' e.g. "2014-05-01T13:00:00Z"
+#' @param congress (numeric) The number of the Congress the committee hearing is taking
+#' place during.
+#' @param chamber (character) The chamber ('house', 'senate', or 'joint') of the committee
+#' holding the hearing.
+#' @param dc (logical) Whether the committee hearing is held in DC \code{TRUE} or in the
+#' field \code{FALSE}
 #' @param bill_ids (numeric) The IDs of any bills mentioned by or associated with the hearing.
 #' @param hearing_type (character) (House only) The type of hearing this is. Can be: 'Hearing',
 #' 'Markup', 'Business Meeting', 'Field Hearing'.
@@ -14,14 +18,17 @@
 #' @template cg
 #' @template cg_query
 #'
-#' @return url (character) (House only) A permalink to that hearing’s description on that committee’s official website.
+#' @return url (character) (House only) A permalink to that hearing's description on that
+#' committee's official website.
 #' @return description (character) A description of the hearing.
-#' @return room (character) If the hearing is in DC, the building and room number the hearing is in. If the hearing is in the field, the address of the hearing.
-#' @return hearing_id (character) (House only) A permalink to that hearing’s description on that committee’s official website.
-#' 
-#' @references 
+#' @return room (character) If the hearing is in DC, the building and room number the hearing
+#' is in. If the hearing is in the field, the address of the hearing.
+#' @return hearing_id (character) (House only) A permalink to that hearing's description on that
+#' committee's official website.
+#'
+#' @references
 #' \url{https://sunlightlabs.github.io/congress/hearings.html}
-#' 
+#'
 #' @examples \dontrun{
 #' cg_hearings(chamber='house', dc=TRUE)
 #' cg_hearings(query='children')
