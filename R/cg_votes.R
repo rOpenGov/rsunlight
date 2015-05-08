f71ea91940fbd145b013aa258fdc3c024a9104d1
Scott Chamberlain <myrmecocystus@gmail.com>
2015-05-08 08:02:14 -0700

    fixed a few man files

--- R/cg_votes.R
+++ R/cg_votes.R
@@ -30,7 +30,7 @@
 #' most commonly used subset of fields only. To save on bandwidth, parsing time, and confusion,
 #' it's recommended to always specify which fields you will be using.
 #' @param key your SunlightLabs API key; loads from .Rprofile
-#' @param return (character) One of table (default), list, or response (httr response object)
+#' @param as (character) One of table (default), list, or response (httr response object)
 #' @param page Page to return. Default: 1. You can use this in combination with the
 #' per_page parameter to get more than the default or max number of results per page.
 #' @param per_page Number of records to return. Default: 20. Max: 50.
--- man-roxygen/districts.R
+++ man-roxygen/districts.R
@@ -1,5 +1,5 @@
 #' @param key your SunlightLabs API key; loads from .Rprofile
-#' @param return (character) One of table (default), list, or response (httr response object)
+#' @param as (character) One of table (default), list, or response (httr response object)
 #' @param ... Optional additional curl options (debugging tools mostly). See examples.
 #' @param page Page to return. Default: 1. You can use this in combination with the
 #' per_page parameter to get more than the default or max number of results per page.
