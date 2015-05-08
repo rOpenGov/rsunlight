d1cd53ad67aacbc0ba54db3434dc1339661ca67d
Scott Chamberlain <myrmecocystus@gmail.com>
2015-05-08 07:56:05 -0700

    fixed man file

--- R/os_legislatorsearch.R
+++ R/os_legislatorsearch.R
@@ -15,7 +15,7 @@
 #' @param per_page Number of records to return. Default: 20. Max: 50.
 #' @param page Page to return. Default: 1. You can use this in combination with the
 #' per_page parameter to get more than the default or max number of results per page.
-#' @param return (character) One of table (default), list, or response (httr response object)
+#' @param as (character) One of table (default), list, or response (httr response object)
 #' @param key your SunlightLabs API key; loads from .Rprofile
 #' @param ... Curl options passed on to \code{\link[httr]{GET}}
 #' @return List of output fields.
